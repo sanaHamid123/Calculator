840fa0bcf0f3150ce7b70689582808d340ae8054
sanaHamid123 <123860189+sanaHamid123@users.noreply.github.com>
2023-09-09 18:26:10 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+function calc(b){
+    var a = document.getElementById('abc')
+    a.value += b
+}
+function result(){
+    var z = document.getElementById('abc')
+   z.value = eval(z.value)
+}
+function clr (){
+    var x = document.getElementById('abc')
+    x.value = " "
+
+}
